c27a69bea761c8ebe0950177f6fe2ad60c0ac27d
Katherine Acosta <kattyacosta28@gmail.com>
2026-03-30 20:13:50 -0600

    Update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +10,59 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/low',
       name: 'low',
-      component: () => import('../views/LowView.vue'),
+      component: () => import('@/views/LowView.vue'),
+    },
+    {
+      path: '/mid',
+      name: 'mid',
+      component: () => import('@/views/MidView.vue'),
+    },
+    {
+      path: '/high',
+      name: 'high',
+      component: () => import('@/views/HighView.vue'),
+    },
+    {
+      path: '/coleccion',
+      name: 'coleccion',
+      component: () => import('@/views/ColeccionView.vue'),
+    },
+    {
+      path: '/carrito',
+      name: 'carrito',
+      component: () => import('@/views/CarritoView.vue'),
+    },
+    {
+      path: '/contacto',
+      name: 'contacto',
+      component: () => import('@/views/ContactoView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      path: '/privacidad',
+      name: 'privacidad',
+      component: () => import('@/views/PrivacidadView.vue'),
+    },
+    {
+      path: '/foro',
+      name: 'foro',
+      component: () => import('@/views/ForoView.vue'),
+    },
+    {
+      path: '/registro',
+      name: 'registro',
+      component: () => import('@/views/RegistroView.vue'),
+    },
+    {
+      path: '/recuperar-contrasena',
+      name: 'recuperar-contrasena',
+      component: () => import('@/views/RecuperarContrasenaView.vue'),
     },
   ],
 })
