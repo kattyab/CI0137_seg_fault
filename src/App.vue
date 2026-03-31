6562f55aef83a1cbb967dd6cadba1f7ae6a668e6
Katherine Acosta <kattyacosta28@gmail.com>
2026-03-30 20:13:07 -0600

    Refactor navigation links in App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,26 @@ import { RouterLink, RouterView } from 'vue-router'
         <li>
           <RouterLink to="/low">Low</RouterLink>
         </li>
+        <li>
+          <RouterLink to="/mid">Mid</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/high">High</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/coleccion">Colección</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/carrito">Carrito</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/contacto">Contacto</RouterLink>
+        </li>
       </ul>
     </nav>
     <div class="search-login">
       <input type="search" placeholder="Buscar Sneakers...">
-      <a href="pages/login.html" class="login-link">Iniciar Sesión</a>
+      <RouterLink to="/login" class="login-link">Iniciar Sesión</RouterLink>
     </div>
   </header>
 
@@ -29,7 +44,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
   <footer>
     <p>Contacto: +506 1234-5678 | charliessneakers@cr.com | Santo Domingo, Heredia</p>
-    <p><a href="pages/contacto.html">Contacto</a> | <a href="pages/privacidad.html">Aviso de Privacidad</a> | © 2026
+    <p><RouterLink to="/contacto">Contacto</RouterLink> | <RouterLink to="/privacidad">Aviso de Privacidad</RouterLink> | © 2026
       Charlie's Sneakers</p>
   </footer>
 </template>
