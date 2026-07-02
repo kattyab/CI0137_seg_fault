984f1abcfe974225eaceda4fc5e4500136844b82
Katty <145637134+kattyab@users.noreply.github.com>
2026-07-01 19:39:51 -0600

    Refactor vitest.config.ts to use configEnv

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,8 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig((configEnv) =>
+  mergeConfig(viteConfig(configEnv), {
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
