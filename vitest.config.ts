9c60b141dacfd4f7c2dd16f2b49734449b13e3bb
Jose Manuel Mora Z <jose.morazuniga@ucr.ac.cr>
2026-07-01 23:39:52 -0600

    Match vitest config in `main`

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,14 +3,11 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default defineConfig((configEnv) =>
-  mergeConfig(
-    viteConfig(configEnv),
-    defineConfig({
-      test: {
-        environment: 'jsdom',
-        exclude: [...configDefaults.exclude, 'e2e/**'],
-        root: fileURLToPath(new URL('./', import.meta.url)),
-      },
-    }),
-  ),
+  mergeConfig(viteConfig(configEnv), {
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
 )
